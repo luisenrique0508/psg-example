054b3950eba21b9c7f6b71b0d6a3dd8222c3e41d
luisenrique0508 <luisenriqueledezmalopez@gmal.com>
2025-05-17 14:45:36 -0400

    ejercicios de los retos session 4

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,34 @@
+
+
+print("""
+  Aca tengo mi juego de cartas favorito, Amigos de Mierda    
+ JUEGO AMIGOS DE MIERDA - REGLAS
+
+Objetivo del juego:
+Ser el primero en deshacerse de todas tus cartas y no terminar como el "amigo de mierda".
+
+Cartas:
+- Se juega con una baraja francesa estándar.
+- Las cartas del 3 al As tienen poder especial.
+- 2: Reinicia la pila (cualquier jugador puede jugar después).
+- 10: Quema la pila (se descartan todas).
+- 3: Carta invisible (no afecta al orden).
+- 7: El siguiente debe jugar una carta menor o igual.
+
+Inicio:
+- Cada jugador recibe 3 cartas en mano, 3 visibles en la mesa y 3 ocultas boca abajo.
+- Se juega primero con la mano, luego con las visibles y por último las ocultas.
+
+Turnos:
+- En tu turno debes jugar una carta igual o mayor a la anterior.
+- Puedes jugar varias cartas iguales a la vez.
+- Si no puedes jugar, debes llevarte toda la pila.
+
+Especial:
+- Cuando te quedes sin cartas en mano, pasas a jugar las visibles.
+- Cuando no tengas visibles, debes jugar las cartas ocultas sin verlas.
+- Si juegas una carta inválida, te llevas la pila.
+
+Final:
+- El último jugador con cartas es el "amigo de mierda".
+""")
